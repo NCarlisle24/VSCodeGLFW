4c38a8c4c0b1dcf6b2424422d3ac7fe90c525731
Nolan Carlisle <143767678+NCarlisle24@users.noreply.github.com>
2024-07-01 16:32:18 -1000

    added VAO, VBOs and shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <fstream>
 
 using namespace std; 
 
@@ -23,13 +24,11 @@ int main()
     GLFWwindow* window = glfwCreateWindow(800, 600, "ZMMR", NULL, NULL);
     if (window == NULL) {
         cout << "Failed to open GLFW window" << endl;
-        glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         cout << "Failed to initialize GLAD" << endl;
         return -1;
     }
@@ -37,9 +36,58 @@ int main()
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f,  -0.5f, 0.0f,
+        0.0f,   0.5f, 0.0f
+    };
+
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3, (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    string vertexShaderSourceText;
+    string currentLine;
+    ifstream vertexFile("shader.vert");
+    while (getline(vertexFile, currentLine)) {
+        vertexShaderSourceText += currentLine;
+        vertexShaderSourceText.push_back('\n');
+    }
+
+    const char* vertexShaderSource = vertexShaderSourceText.c_str();
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    string fragmentShaderSourceText;
+    ifstream fragmentFile("shader.frag");
+    while (getline(fragmentFile, currentLine)) {
+        fragmentShaderSourceText += currentLine;
+        fragmentShaderSourceText.push_back('\n');
+    }
+
+    const char* fragmentShaderSource = fragmentShaderSourceText.c_str();
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
     while(!glfwWindowShouldClose(window)) {
         glfwSwapBuffers(window);
-        glfwPollEvents();    
+        glfwPollEvents();
     }
 
     glfwTerminate();
